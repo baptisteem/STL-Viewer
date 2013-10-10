195700934fb32951ec807b61b06432923f188a53
root <root@localhost.localdomain>
2013-10-10 12:20:29 +0200

    Count the lines in the stl file

--- stl.adb
+++ stl.adb
@@ -1,5 +1,5 @@
-
 with Ada.Text_IO;
+with GNAT.String_Split;
 use Ada.Text_IO;
 
 package body STL is
@@ -10,14 +10,24 @@ package body STL is
    begin
 		Open(File => F, Mode => In_File, Name => Nom_Fichier);
 		-- a faire : compter les facettes (p.ex. chercher les "endloop")...
+		while not End_Of_File(F) loop
+			declare 
+				Line : String := Get_Line(F);
+			begin
+				Nb := Nb + 1;
+			end;
+		end loop;
 		Close(F);
-		return Nb;
+		-- -2 car on supprime les deux lignes d'encadrements
+		-- /7 car 7 lignes par bloc de facette
+		return (Nb-2)/7;
    end;
 
 	function Chargement_ASCII(Nom_Fichier : String) return Maillage is
 		Nb_Facettes : Natural;
 		M : Maillage;
 		F : File_Type;
+		i : Integer := 1;
 	begin
 		Nb_Facettes := Nombre_Facettes(Nom_Fichier);
 		-- une fois qu'on a le nombre de facettes on connait la taille du maillage
@@ -26,6 +36,29 @@ package body STL is
 		-- et remplir le maillage
 		Open(File => F, Mode => In_File, Name => Nom_Fichier);
 		-- a faire...
+	    --while not End_Of_File(F) loop
+		--	declare
+		--		ligne : String := Get_Line(F);
+		--		subs_lignes : GNAT.String_Split.Slice_Set;
+		--		separateur : String := " " & ASCII.HT;
+		--	begin
+				---- On decoupe la chaine grâce au espaces
+				--GNAT.String_Split.Create(subs_lignes,ligne,separateur,Mode => GNAT.String_Split.Multiple);
+				 --
+				----if GNAT.String_Split.Slice(subs_lignes, 2) = "vertex" then
+				----	Put_Line("Vertex");
+				----end if;
+				--for i 1..GNAT.String_Split.Slice_Count(subs_lignes) loop
+				 --       declare
+				 --       	sub_ligne : String := GNAT.String_Split.Slice(subs_lignes, i);
+				 --       begin
+				 --       	Put(sub_ligne);
+				 --       	Put(" ");
+				 --       end;
+				--end loop;
+				--put_Line("");
+			--end;
+		--end loop;
 		Close (F);
 		return M;
 	end;
